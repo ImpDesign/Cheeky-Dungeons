393d68852a4c978b46781923eba1b68f16445916
ImpDesign <impdesign.bm@gmail.com>
2018-07-11 18:23:31 -0600

    Sample Project
    New Unity Project
    Camera Movement Script
    Basic Tile Manager

---
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public float moveSpeed = 5.0f;
+    public float zoomSpeed = 1;
+    public float targetOrtho;
+    public float smoothSpeed = 2.0f;
+    public float minOrtho = 1.0f;
+    public float maxOrtho = 20.0f;
+
+    void Start()
+    {
+        targetOrtho = GetComponent<Camera>().orthographicSize;
+    }
+
+    void Update ()
+    {
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.Translate(new Vector3(moveSpeed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.Translate(new Vector3(-moveSpeed * Time.deltaTime, 0, 0));
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.Translate(new Vector3(0, -moveSpeed * Time.deltaTime, 0));
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.Translate(new Vector3(0, moveSpeed * Time.deltaTime, 0));
+        }
+
+        float scroll = Input.GetAxis("Mouse ScrollWheel");
+        if (scroll != 0.0f)
+        {
+            targetOrtho -= scroll * zoomSpeed;
+            targetOrtho = Mathf.Clamp(targetOrtho, minOrtho, maxOrtho);
+        }
+
+        GetComponent<Camera>().orthographicSize = Mathf.MoveTowards(Camera.main.orthographicSize, targetOrtho, smoothSpeed * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/Managers/TileGenerator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileGenerator : MonoBehaviour
+{
+    public Transform tileObj;
+    public Vector2 startPoint;
+    public Vector2 endPoint;
+
+	// Use this for initialization
+	void Start ()
+    {
+        for (int i = (int)startPoint.x; i < endPoint.x; i++)
+        {
+            for(int j = (int)startPoint.y; j < endPoint.y; j++)
+            {
+                Instantiate(tileObj, new Vector3(i, j, 0), Quaternion.identity);
+            }
+        }
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+}
